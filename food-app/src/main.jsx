fef1113d0effc056c533ab89421a27c96cc52d56
Tharun <tharunkanneboina2002@gmail.com>
2025-12-31 10:41:24 +0530

    rr

--- food-app/src/main.jsx
+++ food-app/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'mdbootstrap/css/mdb.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'mdbootstrap/css/bootstrap.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
